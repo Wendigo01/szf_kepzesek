fceb09936725860af9a96f21212d08a9d3a53167
Wendigo01 <veress.martin@students.jedlik.eu>
2023-11-20 14:07:11 +0100

    Routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const trainings = require('./routes/trainings')
 require("dotenv").config(); // A .env fájlt olvassa
+
 const app = express();
-app.listen(
-  process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`)
-);
+
+app.use('/api/trainings', trainings)
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
