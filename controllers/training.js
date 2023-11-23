697def0777bb9b91941593aa568fe8a20c527afb
Wendigo01 <veress.martin@students.jedlik.eu>
2023-11-23 09:37:10 +0100

    Post és Model

--- controllers/training.js
+++ controllers/training.js
@@ -1,3 +1,5 @@
+const Training = require('../models/Training')
+
 // @desc   Get all trainings
 // @route  GET /api/trainings
 // @access Public
@@ -14,6 +16,7 @@ exports.getTraining = (req, res, next) => {
 // @route  POST /api/trainings
 // @access Private
 exports.createTraining = (req, res, next) => {
+    console.log(req.body);
     res.status(200).json({ success: true, msg: "Új képzés létrehozása" });
 }; 
 // @desc   Update training
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require("express");
-const trainings = require('./routes/trainings')
+const trainings = require("./routes/trainings");
 require("dotenv").config(); // A .env fájlt olvassa
+
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+database.on("error", (error) => {
+  console.log(error);
+});
+database.once("connected", () => {
+  console.log(`Database Connected ${database.host}`);
+});
+
+
 
 const app = express();
 
-app.use('/api/trainings', trainings)
+app.use(express.json())
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.use("/api/trainings", trainings);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
